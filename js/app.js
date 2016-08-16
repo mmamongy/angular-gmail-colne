194c87b91abea43016e132ebf6798b0cd969cc8d
Your Name <you@example.com>
2016-08-16 02:19:07 +0200

    adding cotrollers

---
+++ js/app.js
@@ -0,0 +1,13 @@
+angular.module('myApp', ['ngRoute']).config(function ( $routeProvider ) {
+  'use strict';
+  // configure urls
+  $routeProvider
+    // inbox route
+    .when('/inbox', {
+      templateUrl: 'views/inbox.html',
+      controller: 'InboxCtrl', // map js to html scope
+    })
+    .otherwise({ // default
+      redirectTo: '/inbox'
+    });
+});
---
+++ js/controllers/inbox.js
@@ -0,0 +1,5 @@
+angular.module('myApp').controller('InboxCtrl', 
+		function InboxCtrl(){
+		'use strict';
+		console.log('Inbox Loaded!');
+	});
